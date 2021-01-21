7b0e23cee544e7cc282e133eb3987c7dce5bf035
Peter deHaan <peter@deseloper.com>
2021-01-20 22:52:54 -0800

    Add JavaScript minification

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 const lib = require("../lib")
 
-// const minify = require("html-minifier").minify;
+const minify = require("uglify-js").minify;
 const prettier = require("prettier");
 
 module.exports = (eleventyConfig) => {
@@ -10,8 +10,26 @@ module.exports = (eleventyConfig) => {
       case ".js":
         if (process.env.NODE_ENV === "production") {
           // Return minified content.
-          // TODO: to do this...
-          return content;
+          const opts = {
+            warnings: true,
+            output: {
+              beautify: false
+            }
+          };
+          const result = minify({
+            [outputPath]: content
+          }, opts);
+          if (result.warnings) {
+            console.log(result.warnings.join("\n"));
+          }
+
+          if (result.error) {
+            // Something is horribly wrong, log error and output unminified code.
+            console.error(result.error);
+            process.exitCode = 1;
+            return content;
+          }
+          return result.code;
         }
         // Return prettified (unminified) content.
         return prettier.format(content, { parser: "babel" });
